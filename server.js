d28e2c3f49b0da7840d5435279da3926b48235f4
MAC <mac@DREAMSs-iMac-Pro.local>
2021-08-05 12:32:37 +0530

    make it better

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var path = require('path');
 var express = require('express');
+var connect = require("connect");
+
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, '/dist')));
 app.use(express.static(path.join(__dirname, 'assets')));
 
+var app = connect.createServer().use(connect.static(__dirname + '/dist'));
 
 app.set('port', process.env.PORT || 8080);
 
