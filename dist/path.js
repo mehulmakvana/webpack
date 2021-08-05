c72a40b30d3bd2d6859ab898b92a677ccf0c0287
MAC <mac@DREAMSs-iMac-Pro.local>
2021-08-05 13:08:06 +0530

    make it better

---
+++ dist/path.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+
+    devServer: {
+        // watchContentBase: true,
+        historyApiFallback: true
+    },
+
+  
+
+};
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var path = require('path');
 var express = require('express');
 
+
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.static(path.join(__dirname, 'assets')));
+app.use(express.static(path.join(__dirname, 'path')));
+
 
 
 app.set('port', process.env.PORT || 8080);
