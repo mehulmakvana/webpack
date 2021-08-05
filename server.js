393aab2f4a1bbd8ca71ad6b87ed5fd9be49a0c19
MAC <mac@DREAMSs-iMac-Pro.local>
2021-08-05 12:26:47 +0530

    make it better

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '/dist')));
 app.use(express.static(path.join(__dirname, 'assets')));
 
 
