d7c6c206079a12bbb14eeb0ef60f46d75cee0d25
MAC <mac@DREAMSs-iMac-Pro.local>
2021-08-05 14:43:25 +0530

    make it better

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var root = __dirname + '/dist'
 app.use(express.static(root))
 app.use(fallback('index.html', { root: root }))
 
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 7000);
 
 
 
